2f4761c6951c310394cf6f75a41e15237c36cdc8
Aditya-8788 <adityakhochikar@gmail.com>
2026-03-06 19:51:36 +0530

    remove unnessery part

--- lib/main.dart
+++ lib/main.dart
@@ -7,19 +7,6 @@
    runApp(const MyApp());
  }
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: const StartingPage(),
-//     );
-//   }
-// }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
